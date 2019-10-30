ee5904bf39d6df52f8c0e9c5d2cefa07d5fcb146
Ross Claytor <rossclaytor@MW-RCLAYOTR.local>
2019-10-30 18:21:43 -0500

    create just one sample

--- create_sample.py
+++ create_sample.py
@@ -11,16 +11,12 @@ class NumpyEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, obj)
 
 if __name__=='__main__':
-    gen = model.generator_input(['gs://ross-keras/data/full_test_results.csv'], chunk_size=5000)
+    gen = model.generator_input(['gs://ross-keras/data/full_test_results.csv'], chunk_size=5000, batch_size=1)
     sample = gen.__next__()
 
     input_sample = {}
 
-    input_arrs = []
-    for i in range(0, len(sample[0])):
-        input_arrs.append(sample[0][i][0])
-
-    input_sample['input'] = input_arrs
+    input_sample['input'] = sample[0]
     
     print('Produced sample with label {}'.format(str(sample[1][1])))
 
